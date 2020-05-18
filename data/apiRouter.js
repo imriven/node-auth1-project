52818154fa6c22d4ff7dcc0f7e0be293483bbf3b
Christina Harris <christinathedesigner@outlook.com>
2020-05-18 10:24:18 -0700

    added support to sessions and cookies as well as the global middleware that insures a user is logged in when accessing any route

--- data/apiRouter.js
+++ data/apiRouter.js
@@ -4,25 +4,29 @@ const db = require("./userDb");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 
+
+
 router.post("/register", (req, res) => {
   const credentials = req.body;
   const hash = bcrypt.hashSync(credentials.password, 14);
   credentials.password = hash;
   db.add(credentials)
     .then(res.status(201).send())
-    .catch(res.status(500).json({ error: "error registering user" }));
+    .catch( err => res.status(500).json({ error: "error registering user" }));
 });
 
 router.post("/login", (req, res) => {
   const credentials = req.body;
   db.getByUsername(credentials.username)
-  .then(user => {
-    if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
-    return res.status(401).json({ error: "Incorrect credentials" });
-  }  
-  res.status(200).json({message: "logged in!"})
-  })
-  .catch(res.status(500).json({ error: "error logging in" }));
+    .then((user) => {
+      if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+        return res.status(401).json({ error: "Incorrect credentials" });
+      } else {
+        req.session.username = credentials.username
+        res.status(200).json({ message: "logged in!" });  
+      }
+    })
+    .catch(err =>  res.status(500).json({ error: "error logging in" }));
 });
 
 router.use("/users", UserRouter);
